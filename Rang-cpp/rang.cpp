8520d4f5f2aaeba2ed988043dfce0a7021d6ab94
karu <nsun5274@gmail.com>
2023-05-19 22:58:38 +0900

    05/19 commit

---
+++ Rang-cpp/rang.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <boost/program_options.hpp>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    // 옵션값 비교
+    // boost 옵션 라이브러리 사용할 것
+
+    // 파일 확인
+    // 확장자 맞는지
+    // file을 읽어옴
+
+    // CPP 코드로 변환
+    // 적당히 gcc 돌리기
+
+}
---
+++ Rang-cs/rang.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace Rang.CS;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
